e644a1945eedfb89f248863a4b03605e0f484c92
Jayesh435 <jayeshkaremore07@gmail.com>
2026-07-24 12:06:46 +0530

    chore: update Prisma dependencies and configure PostgreSQL adapter

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,8 +1,23 @@
 import "dotenv/config";
 
-import { PrismaClient }
-from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is required");
+}
+
+const pool = new Pool({
+  connectionString,
+});
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
 
 export default prisma;
